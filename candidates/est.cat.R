cda0bc1c25f9dc572104a90ddfed33ac17c9d1c5
Andres M. Sajo Castelli <asajo@usb.ve>
2014-10-17 20:32:19 -0430

    Start Whish List, and candidates to enter the HEAD

---
+++ candidates/est.cat.R
@@ -0,0 +1,11 @@
+est.cat <- function (collection1, collection2, check.elements = TRUE) {
+        if ( all( names(collection1) != names(collection2) ) & check.elements ) {
+                stop('cannot concatenate two different collections.')
+        }
+        ret = collection1
+        for ( element in names(collection1) ) {
+          cmd = paste0('ret$', element, ' = c(ret$', element, ', collection2$', element,')')
+          eval(parse(text=cmd))
+        }
+        return(ret)
+}
