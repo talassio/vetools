39312858e36c5a42221ba548f3e4359d571e48e1
Andrew Sajo <asajo@usb.ve>
2015-11-23 17:23:02 -0430

    Typo fixes

--- vetools/R/get.shape.state.R
+++ vetools/R/get.shape.state.R
@@ -1,6 +1,7 @@
 # Verified 1.3.18
-# Revision 22/04/2015
-# Version 2.0
+# Revision 06/11/2015
+# Version 2.1
+# TODO: Add DF shape
 # venezuela.estados moved to global constant
 # shape.file = "venezuelaestados"
 # EST <- maptools::readShapeSpatial(paste(system.file("shape",package="vetools"), shape.file, sep="/"))
@@ -11,6 +12,11 @@ function(abb, shape.file="venezuelaestados.rda") {
         if ( missing(abb) ) { return(estados.venezuela) }
         if ( is.na(abb[1]) ) { print(estados.venezuela); return(invisible()) }
         abb <- toupper(abb)
+        if ( "DF" %in% abb ) {
+                warning("DF is not yet integrated. Ignoring this shape request.")
+                abb = abb[abb != "DF"]
+                if ( length(abb) == 0 ) { return(NA) }
+        }
         if ( ! all( abb %in% estados.venezuela$Abb ) ) { stop("Unknown state name. Try get.shape.state(NA).") }
         EST <- NA
         # EST <- maptools::readShapeSpatial(paste(system.file("shape", package="vetools"), shape.file, sep="/"))
